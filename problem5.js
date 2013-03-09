28a4444543386025217df2dcd5f80c5a2ce408b1
Adam Langsner <adamlangsner@gmail.com>
2013-03-09 17:43:26 -0500

    problem 5

---
+++ problem5.js
@@ -0,0 +1,15 @@
+var _ = require("underscore");
+
+var factors = [2520, 11, 13, 16, 17, 19];
+var start = _.reduce(factors, function(memo, num) { return memo * num; }, 1);
+
+var min = start+1;
+for (var i = start; i > 2520; i-=2520) {
+	var guy = _.reduce(factors, function(memo, f) { return memo = memo && i % f == 0; }, true);
+
+	if (guy && i < min) {
+		min = i;
+	}
+}
+
+console.log(min);
